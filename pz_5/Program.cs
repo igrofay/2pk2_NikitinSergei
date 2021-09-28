06b849ffac7b2184642b20ef8366bb19a6ba9ab7
igrof <igrafy@gmail.com>
2021-09-28 15:19:42 +0500

    0

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,7 +6,34 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Задание 1");
+            for(int i = 10; i <= 70; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Задание 2");
+            char a = 'Г';
+            for (int i = 0; i < 8; i+=1)
+            {
+                Console.Write($"{ a++ } ");
+            }
+            Console.WriteLine("\nЗадание 3");
+            for (int i = 0; i< 9; i++)
+            {
+                Console.WriteLine(new string('#', 4));
+            }
+            Console.WriteLine("Задание 4");
+            for (int i = 0; i <= 100; i += 4) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Задание 5");
+            for(int i =5 , j = 64 ;  ; i++ , j--)
+            {
+                Console.WriteLine($"i={i} j={j} разница {j-i}");
+                if (Math.Abs(i - j) == 23) break;
+            }
+           
         }
     }
 }
