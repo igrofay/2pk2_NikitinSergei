97f1f1cf9e487354762e454e29196d17598c1f94
igrof <igrafy@gmail.com>
2021-12-07 18:19:17 +0500

    11

---
+++ pz_17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pz_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] ar = GeomProgres(5);
+            foreach(float i in ar)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static float[] GeomProgres(
+            int n
+        ) {
+            float[] array = new float[n];
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = (float)(10 * Math.Pow(2,i-1));
+            }
+            return array;
+        }
+    }
+}
