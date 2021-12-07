4d546eabe489cec1b048534bf4ab504301a967b9
igrof <igrafy@gmail.com>
2021-12-07 17:31:24 +0500

    1

--- pz_16/Program.cs
+++ pz_16/Program.cs
@@ -7,20 +7,20 @@ namespace pz_16
         static void Main(string[] args)
         {
             float P, S;
-            (P, S) = RectPS(1, 1, 5, 8); // I четверь
+            RectPS(1, 1, 5, 8, out P , out S); // I четверь
             Console.WriteLine($"P={P} , S={S}");
-            (P, S) = RectPS(5, -3, 2, -1); // IV четверь
+            RectPS(5, -3, 2, -1, out P, out S); // IV четверь
             Console.WriteLine($"P={P} , S={S}");
-            (P, S) = RectPS(8, 5, 1, 1); // I четверь
+            RectPS(8, 5, 1, 1, out P , out S); // I четверь
             Console.WriteLine($"P={P} , S={S}");
         }
-        static (float P , float S) RectPS(
+        static void RectPS(
             float x1,float y1,
-            float x2,float y2
+            float x2,float y2,
+            out float P, out float S
         ) {
-            float P = (Math.Abs(x1 - x2) + Math.Abs(y1 - y2)) * 2;
-            float S = Math.Abs(x1 - x2) * Math.Abs(y1 - y2);
-            return (P, S);
+            P = (Math.Abs(x1 - x2) + Math.Abs(y1 - y2)) * 2;
+            S = Math.Abs(x1 - x2) * Math.Abs(y1 - y2);
         }
     }
 }
